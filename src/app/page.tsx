1da1b7b74724de40ed5fc2119018b9f327699c12
Shrawan <shrawanre07@gmail.com>
2024-06-11 19:33:49 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,23 @@
+import HeroSection from "@/components/HeroSection";
+import FeaturedCourses from "@/components/FeaturedCourses";
+import WhyChooseUs from "@/components/WhyChooseUs";
+import MusicSchoolTestimonials from "@/components/TestimonialCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
+import Instructors from "@/components/Instructers";
+import Footer from "@/components/Footer";
+
+
+export default function Home() {
+  return (
+   <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+    <h1 className="text-2xl text-center"> </h1>
+    <HeroSection/>
+    <FeaturedCourses/>
+    <WhyChooseUs/>
+    <MusicSchoolTestimonials/>
+    <UpcomingWebinars/>
+    <Instructors/>
+    <Footer/>
+   </main>
+  );
+}
